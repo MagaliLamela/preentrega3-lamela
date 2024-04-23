eaa803d17694cb30bb360b35727f3f5e3031bbc3
magali.lamela <magalilamelamitsumori@gmail.com>
2024-04-23 00:12:45 -0300

    Creo que ya solucionamos el problema de las rutas!

--- js/buscador.js
+++ js/buscador.js
@@ -55,7 +55,7 @@ if (window.location.pathname.includes("resultados-busqueda.html")) {
         const divProductoFiltrado = document.createElement("div");
         divProductoFiltrado.classList.add("card", "cardIndividual", "col-xl-3", "col-md-6", "col-sm-12", "h-100");
         divProductoFiltrado.innerHTML = `
-        <img src="${producto.imagen}" class="card-img-top mx-auto" alt="${producto.nombre}">
+        <img src=".${producto.imagen}" class="card-img-top mx-auto" alt="${producto.nombre}">
         <h2 class="card-title nombreProducto">${producto.nombre}</h2>
         <h3> <span> $${producto.precioAnterior} </span> | $${producto.precio} </h3>
         <button type="button" class="btnProductos" id="${producto.id}"> Añadir al Carrito </button>
